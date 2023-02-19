0e3d68f98132b10521b1157dc89777a38bcac8a0
TawsifKadir <118630521+TawsifKadir@users.noreply.github.com>
2023-02-19 21:19:56 +0600

    Update main.cpp

--- Task3/main.cpp
+++ Task3/main.cpp
@@ -1,1 +1,27 @@
+//main.cpp
 
+#include <iostream>
+#include "dynArr.h"
+#include <cstdlib>
+using namespace std;
+
+int main(){
+    dynArr arr1;
+    int rows,columns;
+    ::printf("Enter number of rows:");
+    cin >> rows;
+    ::printf("Enter number of columns:");
+    cin >> columns;
+
+    dynArr arr(rows,columns);
+    int a;
+    for(int i=1;i<=rows;i++){
+        for(int j=1;j<=columns;j++){
+            //::printf("Enter element in row %d and column %d:",i,j);
+            cin >> a;
+            arr.setValue(i,j,a);
+        }
+    }
+    arr.printAll();
+    return 0;
+}
