817b9173e8fda0152f5ff23b0957e15b18ac3ccc
TawsifKadir <118630521+TawsifKadir@users.noreply.github.com>
2023-02-19 18:36:01 +0600

    Update main.cpp

--- Task2/main.cpp
+++ Task2/main.cpp
@@ -1,1 +1,13 @@
 //main.cpp
+
+#include <iostream>
+#include "dynArr.h"
+using namespace std;
+
+int main(){
+    dynArr arr1;
+    dynArr arr(5);
+    //int a;
+    arr1.allocate(10);
+    return 0;
+}
