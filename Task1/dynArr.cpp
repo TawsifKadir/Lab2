e978f1275dbc5921745809bc378fb4ae931bc902
TawsifKadir <118630521+TawsifKadir@users.noreply.github.com>
2023-02-19 17:51:10 +0600

    Create dynArr.cpp

---
+++ Task1/dynArr.cpp
@@ -0,0 +1,28 @@
+//dynArr.cpp
+#include "dynArr.h"
+#include <iostream>
+
+using namespace std;
+
+dynArr::dynArr() {
+    data = NULL;
+    size = 0;
+}
+dynArr::dynArr(int a) {
+    data = new int[a];
+    size = a;
+}
+dynArr::~dynArr() {
+    delete[] data;
+}
+int dynArr::getValue(int index) {
+    return data[index];
+}
+void dynArr::setValue(int index, int value) {
+    data[index] = value;
+}
+void dynArr::printAll() {
+    for(int i=0;i<size;i++){
+        cout << getValue(i) << endl;
+    }
+}
